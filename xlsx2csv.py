30ecd9d0f63bf5151b2ed86c30201c252ead30ee
BuildTools <unconfigured@null.spigotmc.org>
2021-08-05 20:10:30 +0300

    code fixes

--- xlsx2csv.py
+++ xlsx2csv.py
@@ -4,13 +4,13 @@ import pandas as pd
 import argparse
 
 
+
 #set arguments from entered paramters
 parser = argparse.ArgumentParser(description='Utility converting xlsx files to csv with optional custom delimiter')
-parser.add_argument('-i', '--input', action="store", help='Input xlsx file')
+parser.add_argument('-i', '--input', action="store", help='Input xlsx file', required=True)
 parser.add_argument('-o', '--output', action="store",  help='Folder to which output file will be saved. If not specified will save in same location as input file (If contains spaces enclose in quotation marks)')
 parser.add_argument('-d', '--delimiter', action="store", help='Define delimiter, if empty default delimiter (comma) will be used, only one character allowed')
 parser.add_argument('-a', '--ascii', action="store_true", help='Flag if delimiter is provided as ASCII code')
-
  # if no parameters entered show help (-h)
 if len(sys.argv[1:])==0:
     parser.print_help()
